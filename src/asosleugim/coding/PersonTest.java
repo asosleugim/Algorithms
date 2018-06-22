a0500db8e8f17ebc4c7b3ce51800887181ffc9f2
Miguel Sosa <miguel.sosa.martinez@gmail.com>
2018-06-22 13:11:36 -0500

    Simple coding

---
+++ src/asosleugim/coding/PersonTest.java
@@ -0,0 +1,34 @@
+package asosleugim.coding;
+
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+
+public class PersonTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Person n1 = new Person(1, "Miguel");
+		Person n2 = new Person(1, "Miguel");
+	
+		
+		
+		System.out.println(n1.getName().equals(n2.getName()));
+		System.out.println(n1==n2);
+		
+		
+		
+		List<String> listOfString = Arrays.asList("London", "Tokyo", "NewYork", "Mexico", "Venezuela", "Germany");
+		Collections.sort(listOfString, Collections.reverseOrder());
+		System.out.println(listOfString); //[Tokyo, NewYork, London]
+
+	
+
+	
+		
+
+	}
+
+}
