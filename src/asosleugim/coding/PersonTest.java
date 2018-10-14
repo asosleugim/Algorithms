a603608a424609c840268227b0ac983b50144b2a
Miguel <asosleugim@hotmail.com>
2018-10-13 17:17:36 -0700

    Test
    Test

--- src/asosleugim/coding/PersonTest.java
+++ src/asosleugim/coding/PersonTest.java
@@ -8,27 +8,27 @@ import java.util.Properties;
 
 public class PersonTest {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Person n1 = new Person(1, "Miguel");
-		Person n2 = new Person(1, "Miguel");
-	
-		
-		
-		System.out.println(n1.getName().equals(n2.getName()));
-		System.out.println(n1==n2);
-		
-		
-		
-		List<String> listOfString = Arrays.asList("London", "Tokyo", "NewYork", "Mexico", "Venezuela", "Germany");
-		Collections.sort(listOfString, Collections.reverseOrder());
-		System.out.println(listOfString); //[Tokyo, NewYork, London]
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		Person n1 = new Person(1, "Miguel");
+//		Person n2 = new Person(1, "Miguel");
+//	
+//		
+//		
+//		System.out.println(n1.getName().equals(n2.getName()));
+//		System.out.println(n1==n2);
+//		
+//		
+//		
+//		List<String> listOfString = Arrays.asList("London", "Tokyo", "NewYork", "Mexico", "Venezuela", "Germany");
+//		Collections.sort(listOfString, Collections.reverseOrder());
+//		System.out.println(listOfString); //[Tokyo, NewYork, London]
 
 	
 
 	
 		
 
-	}
+	//}
 
 }
